c88ec4a55d7f64e3ee6571ca274dda607388c019
Fraser Fernandes <39457210+fraserfdes@users.noreply.github.com>
2021-06-21 16:43:48 +0530

    Add files via upload
    First React app

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox, searchBox } from './components/search-box/search-box.component'
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    }
+  }
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+  handelChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox placeholder='Search monster' handelChange={this.handelChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+
+}
+
+export default App;
